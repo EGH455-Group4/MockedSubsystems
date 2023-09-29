4a5da645189a3cfd404c0d00aca76c478d89265b
Tom Bowyer <tom4310@gmail.com>
2023-09-30 06:21:53 +1000

    Add options request handler

--- air/handler.py
+++ air/handler.py
@@ -7,6 +7,14 @@ from air.helper import ACCEPTABLE_LCD_DISPLAYS
 
 class LcdScreen(Resource):
     '''Used to alter the LCD screen on the sensor'''
+    def options(self):
+        '''The HTTP OPTIONS response'''
+        return None, {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST, OPTIONS',
+            # pylint: disable=C0301
+            'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+        }
 
     def post(self):
         '''The HTTP POST response'''
